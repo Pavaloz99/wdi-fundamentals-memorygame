5c4316fd59538c6dfcc41e90832dfcde025d2c90
Katie Pearlstein <katiepearlstein@Katies-MacBook-Air.local>
2020-03-31 00:37:24 -0700

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,6 +1,16 @@
-let cardOne = "queen";
-let cardTwo = "king";
-let cardThree = "queen";
-let cardFour = "king";
-console.log("User flipped " + cardTwo);
-console.log("User flipped " + cardThree);
+let cards = ['king','queen', 'king', 'queen'];
+cardsInPlay = [];
+let cardOne = cards[0];
+let cardTwo = cards[2];
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+console.log("user flipped " + cardTwo);
+
+if(cardsInPlay.length === 2){
+	if(cardsInPlay[0] === cardsInPlay[1]){
+		alert("You found a match!");
+	} 
+	else{
+		alert("Sorry, try again.");
+	} 
+}
