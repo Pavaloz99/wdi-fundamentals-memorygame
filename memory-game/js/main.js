084f12c45ae4cc9112ee168aeb75701029715574
Katie Pearlstein <katiepearlstein@Katies-MacBook-Air.local>
2020-04-01 14:11:46 -0700

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,27 +1,50 @@
 
-let cards = ['king','queen', 'king', 'queen'];
+let cards = [
+{
+rank: "queen",
+suit: "hearts",
+cardImage: "images/queen-of-hearts.png"
+},
+{
+rank: "queen",
+suit: "diamonds",
+cardImage: "images/queen-of-diamonds.png"
+},
+{
+rank: "king",
+suit: "hearts",
+cardImage: "images/king-of-hearts.png"
+},
+{
+rank: "king",
+suit: "diamonds",
+cardImage: "images/king-of-diamonds.png"
+}
+];
 cardsInPlay = [];
 
 function checkForMatch(){
 	if(cardsInPlay.length === 2){
 	
 		if(cardsInPlay[0] === cardsInPlay[1]){
-		console.log("You found a match!");
+		alert("You found a match!");
 	} 
 		else{
-		console.log("Sorry, try again.");
+		alert("Sorry, try again.");
 		} 
 	}
 }
 
 function flipCard(cardId){
-	cardsInPlay.push(cards[cardId])
+	cardsInPlay.push(cards[cardId].rank);
 
-console.log("User flipped " + cards[cardId]);
+console.log("User flipped " + cards[cardId].rank);
+console.log(cards[cardId].suit);
+console.log(cards[cardId].cardImage);
 
 checkForMatch();
 
 }
 flipCard(0);
-flipCard(1);
+flipCard(2);
 
